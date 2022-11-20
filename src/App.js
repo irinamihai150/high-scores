34059dda9105f5a88e7f5a616d709257801a64ae
irina mihai <irinamihai150@gmail.com>
2022-11-20 20:46:07 +0000

    kkk

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 
 import "./App.css";
 import HighScoreTable from "./HighScoreTable";
-import Button from "./Button";
+
 
 function App() {
   return (
     <div>
-    <Button className ="btn btn-primary"/>
       <HighScoreTable/>
     </div>
 
